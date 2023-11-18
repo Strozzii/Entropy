a8162321912c0fba2cf38a7137d55b8b3bc6a174
Strozzii <stroz.dennis@gmail.com>
2023-11-18 17:30:49 +0100

    :art: 0 Clear main

--- main.c
+++ main.c
@@ -8,16 +8,5 @@
 
 int main() {
 
-	char in[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxX";
-	char out[50] = {0};
-
-	test_rd_chars_wr_bits(in, out);
-
-	printf("%s", out);
-
-	//printf("%d\n", strcmp(out, "####"));
-
-
-
 	return 0;
 }
