ace24183bbbab297804e2a17d053a5797c740d15
Strozzii <stroz.dennis@gmail.com>
2023-11-19 10:23:36 +0100

    :white_check_mark: Added tests from moodle

--- main.c
+++ main.c
@@ -37,22 +37,44 @@
  */
 int main()
 {
+	int output;
 
-	char in[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxX";
-	char out[50] = { 0 };
+	char test_input_1[] = "";
+	char test_output_1[50] = {0};
+	test_rd_chars_wr_bits(test_input_1, test_output_1);
+	output = strcmp(test_output_1, "");
+	printf("%s\n", output ? "Testfall 1 fehlgeschlagen!" : "Testfall 1 erfolgreich!");
 
-	test_rd_chars_wr_bits(in, out);
+	char test_input_2[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxX";
+	char test_output_2[50] = {0};
+	test_rd_chars_wr_bits(test_input_2, test_output_2);
+	output = strcmp(test_output_2, "UUUUUU");
+	printf("%s\n", output ? "Testfall 2 fehlgeschlagen!" : "Testfall 2 erfolgreich!");
 
-	printf("%s\n", out);
+	char test_input_3[] = "abCdefGHijKlmnOPqrStuvWXyzAbcdEF";
+	char test_output_3[50] = {0};
+	test_rd_chars_wr_bits(test_input_3, test_output_3);
+	output = strcmp(test_output_3, "####");
+	printf("%s\n", output ? "Testfall 3 fehlgeschlagen!" : "Testfall 3 erfolgreich!");
 
-	char in2[] = "abCdefGHijKlmnOPqrStuvWXyzAbcdEF";
-	char out2[50] = { 0 };
+	char test_input_4[] = "";
+	char test_output_4[50] = {0};
+	test_rd_bits_wr_chars(test_input_4, test_output_4);
+	output = strcmp(test_output_4, "");
+	printf("%s\n", output ? "Testfall 4 fehlgeschlagen!" : "Testfall 4 erfolgreich!");
 
-	test_rd_chars_wr_bits(in2, out2);
+	char test_input_5[] = "1234567890";
+	char test_output_5[50] = {0};
+	test_rd_bits_wr_chars(test_input_5, test_output_5);
+	output = strcmp(test_output_5, "00110001001100100011001100110100001101010011011000110111001110000011100100110000");
+	printf("%s\n", output ? "Testfall 5 fehlgeschlagen!" : "Testfall 5 erfolgreich!");
 
-	printf("%s\n", out2);
+	char test_input_6[] = "0192837465";
+	char test_output_6[50] = {0};
+	test_rd_bits_wr_chars(test_input_6, test_output_6);
+	output = strcmp(test_output_6, "00110000001100010011100100110010001110000011001100110111001101000011011000110101");
+	printf("%s\n", output ? "Testfall 6 fehlgeschlagen!" : "Testfall 6 erfolgreich!");
 
-	printf("%d\n", strcmp(out2, "####"));
 
 	return 0;
 }
